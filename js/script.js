195a91a8b67ad93288549911b4d62acd7f42efb6
java <shauriramadhan01@gmail.com>
2024-01-13 14:29:42 +0700

    membuat toko tijoepass

---
+++ js/script.js
@@ -0,0 +1,49 @@
+//toggle class active
+
+const navbarNav = document.querySelector(".navbar-nav");
+
+//ketika hamburger menu dkilik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+//toogle class active ketika untuk shopping cart
+const shoppingCart = document.querySelector(".shopping-cart");
+document.querySelector("#shopping-cart-button").onclick = (e) => {
+  shoppingCart.classList.toggle("active");
+  e.preventDefault();
+};
+
+//klik diluar sidebar untuk menghilangkan nav
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
+
+// modall box
+const itemDetailModal = document.querySelector("#item-detail-modal");
+const itemDetailButtons = document.querySelectorAll(".item-detail-button");
+
+itemDetailButtons.forEach((btn) => {
+  btn.onclick = (e) => {
+    itemDetailModal.style.display = "flex";
+    e.preventDefault();
+  };
+});
+
+// klik tombol close modal
+document.querySelector(".modal .close-icon").onclick = (e) => {
+  itemDetailModal.style.display = "none";
+  e.preventDefault();
+};
+
+//klik diluar modal
+const modal = document.querySelector("#item-detail-modal");
+window.onclick = (e) => {
+  if (e.target === modal) {
+    modal.style.display = "none";
+  }
+};
---
+++ src/app.js
@@ -0,0 +1,145 @@
+document.addEventListener("alpine:init", () => {
+  Alpine.data("products", () => ({
+    items: [
+      {
+        id: 1,
+        name: "Compass Gazelle Hi Black White",
+        img: "products 1.png",
+        price: 300000,
+      },
+      {
+        id: 2,
+        name: "Compass️ Proto Lite Blue",
+        img: "product 2.png",
+        price: 300000,
+      },
+      {
+        id: 3,
+        name: "Compass Velocity Black",
+        img: "product 3.png",
+        price: 300000,
+      },
+      {
+        id: 4,
+        name: "Compass Retrograde Hi Creme Grape",
+        img: "product 4.png",
+        price: 300000,
+      },
+      {
+        id: 5,
+        name: "Compass️ Proto Lite Sage Green",
+        img: "product 5.png",
+        price: 300000,
+      },
+    ],
+  }));
+
+  Alpine.store("cart", {
+    items: [],
+    total: 0,
+    quantity: 0,
+    add(newItem) {
+      //cek apakah ada barang yang sama di cart
+      const cartItem = this.items.find((item) => item.id === newItem.id);
+
+      //jika belum ada (cart masih kosong)
+      if (!cartItem) {
+        this.items.push({ ...newItem, quantity: 1, total: newItem.price });
+        this.quantity++;
+        this.total += newItem.price;
+      } else {
+        //jika barang sudah ada , cek apakah barang beda atau sama
+        this.items = this.items.map((item) => {
+          //jika barang berbeda
+          if (item.id !== newItem.id) {
+            return item;
+          } else {
+            //jika barang sudah ada , tambah quantity dan total
+            item.quantity++;
+            item.total = item.price * item.quantity;
+            this.quantity++;
+            this.total += item.price;
+            return item;
+          }
+        });
+      }
+    },
+    remove(id) {
+      //ambil item yang mau di remove berdasarkan idnya
+      const cartItem = this.items.find((item) => item.id === id);
+
+      //jika item lebih dari satu
+      if (cartItem.quantity > 1) {
+        //telusuri 1 1
+        this.items = this.items.map((item) => {
+          //jika bukan barang yang diklik
+          if (item.id !== id) {
+            return item;
+          } else {
+            item.quantity--;
+            item.total = item.price * item.quantity;
+            this.quantity--;
+            this.total -= item.price;
+            return item;
+          }
+        });
+      } else if (cartItem.quantity === 1) {
+        //jika barangnya sisa 1
+        this.items = this.items.filter((item) => item.id !== id);
+        this.quantity--;
+        this.total -= cartItem.price;
+      }
+    },
+  });
+});
+
+//form validation
+const checkoutButton = document.querySelector(".checkout-button");
+checkoutButton.disabled = true;
+
+const form = document.querySelector("#checkoutForm");
+form.addEventListener("keyup", function () {
+  for (let i = 0; i < form.elements.length; i++) {
+    if (form.elements[i].value.length !== 0) {
+      checkoutButton.classList.remove("disabled");
+      checkoutButton.classList.add("disabled");
+    } else {
+      return false;
+    }
+  }
+  checkoutButton.disabled = false;
+  checkoutButton.classList.remove("disabled");
+});
+
+//kirim data ketika chekcout diklik
+checkoutButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const data = new URLSearchParams(formData);
+  const objData = Object.fromEntries(data);
+  const massage = formatMassage(objData);
+  window.open("http://wa.me/6287762627423?text=" + encodeURIComponent(massage));
+});
+
+//format pesan wa
+const formatMassage = (obj) => {
+  return `Data Customer
+    Nama: ${obj.name}
+    Email: ${obj.email}
+    No HP: ${obj.phone}
+
+Data Pesanan
+    ${JSON.parse(obj.items).map(
+      (item) => `${item.name} (${item.quantity} x ${rupiah(item.total)}) \n`
+    )}
+    TOTAL: ${rupiah(obj.total)}
+    terima kasih.`;
+};
+//konversi ke rupiah
+const rupiah = (number) => {
+  return new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+    maximumFractionDigits: 0,
+  }).format(number);
+};
